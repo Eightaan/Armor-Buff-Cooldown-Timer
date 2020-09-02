4fd35d0a96a07b8ff56c2754d3d8cda199ed1834
Eightaan <57832730+Eightaan@users.noreply.github.com>
2020-09-02 10:47:41 +0200

    Add files via upload

--- Armor Buff Cooldown Timer/hudmanagerpd2.lua
+++ Armor Buff Cooldown Timer/hudmanagerpd2.lua
@@ -2,19 +2,18 @@ local function format_seconds(raw)
 	return string.format("%02d",math.floor(raw % 60))
 end
 
-Hooks:PreHook(HUDManager,"_create_teammates_panel","khud_hudmanager_create_teammates_panel",function(self,hud)
+Hooks:PreHook(HUDManager,"_create_teammates_panel","buff_hudmanager_create_teammates_panel",function(self, hud)
 	hud = hud or managers.hud:script(PlayerBase.PLAYER_INFO_HUD_PD2)
-	self._khud_buffs_master = hud.panel:panel({
-		name = "khud_buffs_master",
-		halign = "grow",
-		valign = "bottom",
+	self._buffs_master = hud.panel:panel({
+		name = "buffs_master",
+		x = 1105,
+		y = 679,
 		w = hud.panel:w(),
 		h = hud.panel:h()
 	})
 end)
 
-Hooks:PostHook(HUDManager,"update","khud_hudmanager_update",function(self,t,dt)
-	local hud_panel = self._teammate_panels[HUDManager.PLAYER_PANEL]
+Hooks:PostHook(HUDManager,"update","buff_hudmanager_update",function(self, ...)
 	local _t = Application:time()
 	local player_unit = managers.player and managers.player:local_player()
 	if not (player_unit) then 
@@ -24,64 +23,73 @@ Hooks:PostHook(HUDManager,"update","khud_hudmanager_update",function(self,t,dt)
 	
 	local buffs = managers.player:get_buffs()
 
-	local buffs_parent_panel = hud_panel._khud_buffs_panel
+	local buffs_parent_panel = self._buffs_master
 	local buff_panel
 	local buff_label
 	local text = ""
 	local buff_data = managers.player:get_buff_data()
 	local time_left = 0
-	local buff_num = 1
-	local queued_remove_buffs = {} --todo global this
+	local queued_remove_buffs = {}
 	
 	for id, buff in pairs(buffs) do
 		buff_panel = buffs_parent_panel:child(id)
 		buff_label = buff_panel:child("label")	
-			if not buff.end_t then 
-				table.insert(queued_remove_buffs,id)
-				break --go directly to jail, do not pass go, do not collect $200
-			end
-			time_left = buff.end_t - _t
-			text = format_seconds(1 + math.floor(time_left))
-			if buff.end_t < _t and not buff_data[id].persistent_timer then 
-				table.insert(queued_remove_buffs,id)
+		if not buff.end_t then 
+			table.insert(queued_remove_buffs,id)
+			break
+		end
+		
+		time_left = buff.end_t - _t
+		text = format_seconds(1 + math.floor(time_left))
+		if buff.end_t < _t and not buff_data[id].persistent_timer then 
+			table.insert(queued_remove_buffs,id)
+		else
+			local invuln_timer = player_unit:character_damage()._can_take_dmg_timer
+			if invuln_timer and invuln_timer > 0 then 
+				buff_label:set_text(text)
+				buff_label:set_color(Color("66ff99"))
+				buff_label:set_alpha(1)
 			else
-
-					local invuln_timer = player_unit:character_damage()._can_take_dmg_timer
-					if invuln_timer and invuln_timer > 0 then 
-						buff_label:set_text(text)
-						buff_label:set_color(Color("FFFFFF"))
-						buff_label:set_alpha(1)
-					else
-						buff_label:set_text(text)
-						buff_label:set_color(Color("FFFFFF"))
-						buff_label:set_alpha(1)
-					end 
-
-			end	
-			
-		buff_panel:set_x(buff_panel:w() * math.floor(buff_num / 6)) --6 is max_per_column
-		buff_panel:set_y(buffs_parent_panel:h() - ((buff_num % 6) * (buff_panel:h() + 10))) --6 is max_per_column; todo make pad small with more buffs
-
-		buff_num = buff_num + 1 ---this feels wrong
+				buff_label:set_text(text)
+				buff_label:set_color(Color("ff6666"))
+				buff_label:set_alpha(1)
+			end 
+		end	
 	end
 	
 	for n,buffid in ipairs(queued_remove_buffs) do
-		managers.player:remove_buff(buffid) --doing this cause it's probably a bad idea to modify the table you're iterating through otherwise
+		managers.player:remove_buff(buffid)
 	end
 
 end)
 
 --trackers
 function HUDManager:add_buff(id)
-	local hud = self._teammate_panels[HUDManager.PLAYER_PANEL]
-	hud:_add_buff(id)
+	local buffs_panel = self._buffs_master
+	local buff_data = managers.player:get_buff_data()[id]
+	local current_buffs = managers.player:get_buffs()
+	local buff = current_buffs[id]
+
+	local label = buff.label or buff_data.label
+	
+	if buffs_panel:child(id) then
+			buffs_panel:child(id):child("label"):set_color(text_color)
+	else
+		local buff_panel = buffs_panel:panel({
+			name = tostring(id)
+		})
+		local buff_label = buff_panel:text({
+			name = "label",
+			font_size = 16,
+			font = tweak_data.hud.medium_font_noshadow,
+			text = label
+		})	
+	end
 end
 
 function HUDManager:remove_buff(id)
-	local hud = self._teammate_panels[HUDManager.PLAYER_PANEL]
-	hud:_remove_buff(id)
-end
-
-function HUDManager:layout_khud_buffs(params)
-	self._teammate_panels[HUDManager.PLAYER_PANEL]:_layout_khud_buffs(params)
+	local buffs_panel = self._buffs_master
+	if buffs_panel:child(id) then
+		buffs_panel:remove(buffs_panel:child(id))
+	end
 end
--- Armor Buff Cooldown Timer/playermanager.lua
+++ Armor Buff Cooldown Timer/playermanager.lua
@@ -1,15 +1,11 @@
 _G.ArmorBuff = {}
 
-Hooks:PostHook(PlayerManager,"init","khud_init_playermanager",function(self)
+Hooks:PostHook(PlayerManager,"init","buff_init_playermanager",function(self)
 	self._player_buffs = {}
 end)
 
-Hooks:PostHook(PlayerManager,"_internal_load","khud_player_internal_load",function(self)
-	managers.hud:layout_khud_buffs()
-end)
-
-function PlayerManager:add_buff(id,params) --custom function to register + add buffs
-	if not (managers.hud and id) then return end --in case it's called from menu
+function PlayerManager:add_buff(id,params)
+	if not (managers.hud and id) then return end
 	params = params or {}
 	local current_buffs = self:get_buffs()
 	local t = Application:time()
@@ -22,14 +18,7 @@ function PlayerManager:add_buff(id,params) --custom function to register + add b
 	if params.duration then
 		current_buffs[id].end_t = (t + params.duration or buff_data.duration or 5)
 	end
-	managers.hud:add_buff(id)	--create panel
-end
-
-function PlayerManager:clear_all_buffs()
-	for id,buff in pairs(self:get_buffs()) do
-		managers.hud:remove_buff(id)
-	end
-	self._player_buffs = {}
+	managers.hud:add_buff(id)
 end
 
 function PlayerManager:remove_buff(id)
@@ -51,7 +40,7 @@ function PlayerManager:get_buffs()
 	return self._player_buffs
 end
 
-Hooks:PostHook(PlayerManager,"activate_temporary_upgrade","khud_activate_temporary_upgrade",function(self,category, upgrade)
-	local t = self._temporary_upgrades[category] and self._temporary_upgrades[category][upgrade] and self._temporary_upgrades[category][upgrade].expire_time
-	self:add_buff(upgrade,{end_t = t}) --i'd have to re-calculate stuff or overwrite the function to use duration instead of end_t. i'd rather risk having desynced end times
+Hooks:PostHook(PlayerManager,"activate_temporary_upgrade","buff_activate_temporary_upgrade",function(self, category, upgrade)
+	local t = self._temporary_upgrades[category] and self._temporary_upgrades["temporary"]["armor_break_invulnerable"] and self._temporary_upgrades["temporary"]["armor_break_invulnerable"].expire_time
+	self:add_buff(upgrade,{end_t = t})
 end)
